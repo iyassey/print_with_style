69e4764c37807198f701415d675c0ae9268d1062
Thea <uythea487102@gmail.com>
2024-03-23 20:48:39 +0800

    Define the font style of the variables and Displayed the inputted data

--- print_with_style.py
+++ print_with_style.py
@@ -3,6 +3,9 @@
 #this program will ask the user for their name, dream job, hobby, personality type, and motto
 #this program will also display the data inputted by the user
 
+#import modules
+import pyfiglet
+
 #ask the user for their name, dream job, hobby, personality type, and motto
 name = input("Enter your name: ")
 dream_job = input("Enter your dream job: ")
@@ -14,13 +17,24 @@ motto = input("Enter your motto: ")
 message = ("You will reach your dreams, Believe it!")
 
 #define the font style of the variables
+name = (pyfiglet.figlet_format(name,font='invita'))
+dream_job = (pyfiglet.figlet_format(dream_job,font='invita'))
+hobby = (pyfiglet.figlet_format(hobby,font='contessa'))
+personality_type = (pyfiglet.figlet_format(personality_type,font='contessa'))
+motto = (pyfiglet.figlet_format(motto,font='digital'))
+message = (pyfiglet.figlet_format(message,font='digital'))
 
 
 #define the font color of the variables
 
 
 #display the data
-
+print(name)
+print(dream_job)
+print(hobby)
+print(personality_type)
+print(motto)
+print(message)
 
 #this is the end of the program!
 #thank you for using my program!
