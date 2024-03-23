0244b3f5ad9b593fce7d22f7dcfc862e1a40db24
Thea <uythea487102@gmail.com>
2024-03-23 20:34:03 +0800

    Created the flow of the program using pseudocode

---
+++ print_with_style.py
@@ -0,0 +1,22 @@
+#author__thea_uy
+#date__march_23_2024
+#this program will ask the user for their name, dream job, hobby, personality type, and motto
+#this program will also display the data inputted by the user
+
+#ask the user for their name, dream job, hobby, personality type, and motto
+
+
+#add a message for the user
+
+
+#define the font style of the variables
+
+
+#define the font color of the variables
+
+
+#display the data
+
+
+#this is the end of the program!
+#thank you for using my program!
